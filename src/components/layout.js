d04c3bd0eb569be10c155c4ab92ffbc8f6bb5adc
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-03-09 08:32:35 +0100

    Hero Image

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,11 +33,8 @@ const Layout = ({ children }) => (
           }}
         >
           <main>{children}</main>
-          {/* <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer> */}
+          <footer>
+          </footer>
         </div>
       </>
     )}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Layout from "../components/layout"
 //import Article from "../components/article/article"
+import heroImage from "../images/heroImage.jpg"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <div>
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <div style={{ backgroundColor: 'grey', height: '80vh' }}></div>
+      <img src={heroImage} alt="Hero Image" style={{ height: '80vh'}}/>
     </Layout>
   </div>
 )
