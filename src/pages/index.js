83e71d9d60ec9c9a8f3e3c735d68c7343581bc46
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-03-09 08:49:31 +0100

    Articles in Index eingebunden

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,12 @@ import React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Layout from "../components/layout"
-//import Article from "../components/article/article"
+import Article from "../components/article/article"
 import heroImage from "../images/heroImage.jpg"
+import codeImage from "../images/code.jpeg"
+import girlImage from "../images/girl.jpeg"
+import meetingImage from "../images/meeting.jpeg"
+import pupilsImage from "../images/pupils.jpeg"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -12,6 +16,14 @@ const IndexPage = () => (
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <img src={heroImage} alt="Hero Image" style={{ height: '80vh'}}/>
     </Layout>
+    <div className="container">
+      <div className="mt-5 d-flex flex-row flex-wrap justify-content-around">
+        <Article title="Title" excerpt="lorem ipsumjllskhls" summary="ljlskjlskdl" link="https://www.orf.at" img={codeImage}></Article>
+        <Article title="Title" excerpt="lorem ipsumjllskhls" summary="ljlskjlskdl" link="https://www.orf.at" img={girlImage}></Article>
+        <Article title="Title" excerpt="lorem ipsumjllskhls" summary="ljlskjlskdl" link="https://www.orf.at" img={meetingImage}></Article>
+        <Article title="Title" excerpt="lorem ipsumjllskhls" summary="ljlskjlskdl" link="https://www.orf.at" img={pupilsImage}></Article>
+      </div>
+    </div>
   </div>
 )
 
